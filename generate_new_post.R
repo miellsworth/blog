1e7e97377d044bcb633adf8c3a5a5c1c7404e12b
Michael Ellsworth <michael.ellsworth7@gmail.com>
2024-04-20 21:04:07 -0600

    new post 2024-04-20

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,6 +1,6 @@
 source("new_post.R")
 
 new_post(
-  title = "Re-creating a Shiny App for TidyTuesday plots",  # Title of the blog post
-  description = "Can I re-create Nicola Rennie's Shiny App?"  # Description of the blog post
+  title = "5 Weeks - Day 1",  # Title of the blog post
+  description = "A personal challenge to build a portfolio"  # Description of the blog post
   )
