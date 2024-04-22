4bad9851f9c3c9d3a344fc51c60283c30ea82dd1
Michael Ellsworth <michael.ellsworth7@gmail.com>
2024-04-21 21:31:27 -0600

    update generate_new_post

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,6 +1,7 @@
 source("new_post.R")
 
 new_post(
-  title = "5 Weeks - Day 1",  # Title of the blog post
-  description = "A personal challenge to build a portfolio"  # Description of the blog post
+  title = "5 Weeks - Day 2",  # Title of the blog post
+  description = "We doing this"  # Description of the blog post
   )
+
