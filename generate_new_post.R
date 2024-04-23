906f0accba51b341855678b8a6803a1722d85325
Michael Ellsworth <michael.ellsworth7@gmail.com>
2024-04-23 06:30:55 -0600

    new draft post 2024-04-23

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,7 +1,7 @@
 source("new_post.R")
 
 new_post(
-  title = "5 Weeks - Day 3",  # Title of the blog post
-  description = "Mondays!"  # Description of the blog post
+  title = "5 Weeks - Day 4",  # Title of the blog post
+  description = "Radio and Quarto"  # Description of the blog post
   )
 
