f94b01e4099b13120541c3e0b70e8d97e4383539
Michael Ellsworth <michael.ellsworth7@gmail.com>
2024-04-28 07:52:31 -0600

    new post 2024-04-28

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,7 +1,6 @@
 source("new_post.R")
 
 new_post(
-  title = "5 Weeks - Day 8",  # Title of the blog post
-  description = "Saturdays are for the blogs"  # Description of the blog post
+  title = "5 Weeks - Day 9",  # Title of the blog post
+  description = "Sunday Choresday"  # Description of the blog post
   )
-
