c8056f89cdec0548d40e50210a6006a8211678d4
Michael Ellsworth <michael.ellsworth7@gmail.com>
2024-04-29 08:26:03 -0600

    update generate_new_post

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,6 +1,6 @@
 source("new_post.R")
 
 new_post(
-  title = "5 Weeks - Day 9",  # Title of the blog post
-  description = "Sunday Choresday"  # Description of the blog post
+  title = "5 Weeks - Day 10",  # Title of the blog post
+  description = "Double Digits!"  # Description of the blog post
   )
