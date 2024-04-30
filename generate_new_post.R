74dc418beb465500cc69fd524c865c7a4f97c3aa
Michael Ellsworth <michael.ellsworth7@gmail.com>
2024-04-30 13:38:10 -0600

    new post 2024-04-30

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,6 +1,6 @@
 source("new_post.R")
 
 new_post(
-  title = "5 Weeks - Day 10",  # Title of the blog post
-  description = "Double Digits!"  # Description of the blog post
+  title = "5 Weeks - Day 11",  # Title of the blog post
+  description = "YYC Weather"  # Description of the blog post
   )
