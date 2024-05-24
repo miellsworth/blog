fd3631eb0301c7d22ccf6b584297b8c3b5440be5
Michael Ellsworth <michael.ellsworth7@gmail.com>
2024-05-24 07:29:59 -0600

    new post 2024-05-24

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,6 +1,6 @@
 source("new_post.R")
 
 new_post(
-  title = "5 Weeks - Day 34",  # Title of the blog post
+  title = "5 Weeks - Day 35",  # Title of the blog post
   description = "TBD"  # Description of the blog post
   )
