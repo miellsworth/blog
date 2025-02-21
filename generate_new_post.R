05fca4fd4a6be3abf769c96030e3ff54a200cd58
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-02-21 12:54:34 -0700

    new recipes

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,7 +1,8 @@
 source("new_post.R")
 
 new_post(
-  title = "Grilled Shrimp With Chile and Garlic",  # Title of the blog post
+  title = "Creamy Tortellini Soup",  # Title of the blog post
   directory = "cooking",  # Directory to save the blog post
-  description = "BBQ shrimp"  # Description of the blog post
+  description = "Creamy Tortellini"  # Description of the blog post
   )
+ 
