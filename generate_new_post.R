66c45a03ffb38429badc73d2ad62081c16a0b0f7
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-02-24 12:59:33 -0700

    new recipes

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,8 +1,8 @@
 source("new_post.R")
 
 new_post(
-  title = "Creamy Tortellini Soup",  # Title of the blog post
+  title = "Sheet-pan gnocchi w/ roasted tomatoes",  # Title of the blog post
   directory = "cooking",  # Directory to save the blog post
-  description = "Creamy Tortellini"  # Description of the blog post
+  description = ""  # Description of the blog post
   )
  
