73a39b1dec8bf3597d64ef68ac88b7bb6842b6ef
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-05-31 11:45:34 -0600

    new recipes

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,7 +1,7 @@
 source("new_post.R")
 
 new_post(
-  title = "Simple smoothie",  # Title of the blog post
+  title = "Energy bites",  # Title of the blog post
   directory = "cooking",  # Directory to save the blog post
   description = ""  # Description of the blog post
   )
