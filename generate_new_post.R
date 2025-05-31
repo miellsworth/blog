26e1098e3ee45b67a58db089be80ba8fdfc765b9
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-05-31 11:28:38 -0600

    update and create new recipes

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,7 +1,7 @@
 source("new_post.R")
 
 new_post(
-  title = "Sheet-pan gnocchi w/ roasted tomatoes",  # Title of the blog post
+  title = "Simple smoothie",  # Title of the blog post
   directory = "cooking",  # Directory to save the blog post
   description = ""  # Description of the blog post
   )
