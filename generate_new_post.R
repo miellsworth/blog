41eb3405096ff3f8b33afc14aee6b89587805c18
Michael Ellsworth <michael.ellsworth7@gmail.com>
2025-11-17 12:42:21 -0700

    update data blog with 2023-07-25 tidy tuesday notes

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,8 +1,7 @@
 source("new_post.R")
 
 new_post(
-  title = "Energy bites",  # Title of the blog post
-  directory = "cooking",  # Directory to save the blog post
+  title = "Notes on Tidy Tuesday - 2023-07-25",  # Title of the blog post
+  directory = "data",  # Directory to save the blog post
   description = ""  # Description of the blog post
   )
- 
