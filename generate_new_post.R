5b2c3a1d6e926cecb92d5a855ebc3571c5ec3cb5
Michael Ellsworth <michael.ellsworth7@gmail.com>
2026-01-08 07:34:51 -0700

    new notes on tidy tuesday 2026-01-06

--- generate_new_post.R
+++ generate_new_post.R
@@ -1,7 +1,7 @@
 source("new_post.R")
 
 new_post(
-  title = "Notes on Tidy Tuesday - 2024-01-09",  # Title of the blog post
+  title = "Notes on Tidy Tuesday - 2026-01-06",  # Title of the blog post
   directory = "data",  # Directory to save the blog post
   description = ""  # Description of the blog post
   )
